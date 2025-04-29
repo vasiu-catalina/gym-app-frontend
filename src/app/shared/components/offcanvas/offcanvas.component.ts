8c8dabc02dbb465efe0769dd2e76d3641d3d8cc0
= <vasiu.catalina10@gmail.com>
2025-04-29 15:20:47 +0300

    add offcanvas

---
+++ src/app/shared/components/offcanvas/offcanvas.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-offcanvas',
+  imports: [],
+  templateUrl: './offcanvas.component.html',
+  styleUrl: './offcanvas.component.css'
+})
+export class OffcanvasComponent {
+
+}
