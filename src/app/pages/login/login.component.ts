2131c3571f655fc15514b869ce58f0eae62a0f96
= <vasiu.catalina10@gmail.com>
2025-04-29 13:31:51 +0300

    add login component

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../shared/services/auth.service';
+import { AuthState } from '../../shared/states/auth.state';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  error: string | null = null;
+  loading = false;
+
+  form !: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private authState: AuthState
+  ) { }
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.form.invalid) return;
+
+    this.loading = true;
+    this.error = null;
+
+    const { email, password } = this.form.value;
+
+    this.authService.login({ email, password }).subscribe({
+      next: (res) => {
+        this.authState.setToken(res.token);
+        this.loading = false;
+      },
+      error: (err) => {
+        this.error = err.error?.message || 'Login failed';
+        this.loading = false;
+      }
+    });
+  }
+}
